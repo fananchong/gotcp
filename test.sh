a0ede905ce442ef16530e42154c7046d6ff47479
fananchong <fananchong@126.com>
2019-07-19 12:09:42 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -ex
+
+pkill -9 client
+
+cd bin
+
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+nohup ./client > /dev/null 2>&1 &
+
+./server
+
+
